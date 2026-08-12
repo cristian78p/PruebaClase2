2886ff2dfc85a44fd048ab52d5923ca550768cb6
cristian78p <ccreyb@gmail.com>
2026-08-12 13:13:37 -0500

    Add distance calculation for uva roja

--- frutas_app.py
+++ frutas_app.py
@@ -19,6 +19,7 @@ st.write("Vector de tu fruta:", fruta_usuario)
 manzana = [170, 7.0, 7]
 banano = [120, 5.0, 9]
 naranja = [200, 8.0, 6]
+uvaRoja = [6, 2.5, 8]
 
 # Calculamos las distancias
 
@@ -39,6 +40,11 @@ distancia_naranja = math.sqrt(
     (fruta_usuario[1] - naranja[1])**2 +
     (fruta_usuario[2] - naranja[2])**2
 )
+distancia_uva = math.sqrt(
+    (fruta_usuario[0] - uvaRoja[0])**2 +
+    (fruta_usuario[1] - uvaRoja[1])**2 +
+    (fruta_usuario[2] - uvaRoja[2])**2
+)
 
 # Mostramos las distancias
 st.subheader("Distancias")
@@ -46,12 +52,14 @@ st.subheader("Distancias")
 st.write("🍎 Manzana:", distancia_manzana)
 st.write("🍌 Banano:", distancia_banano)
 st.write("🍊 Naranja:", distancia_naranja)
+st.write("🍊 Naranja:", distancia_uva)
 
 # Buscamos la distancia menor
 distancias = {
     "🍎 Manzana": distancia_manzana,
     "🍌 Banano": distancia_banano,
-    "🍊 Naranja": distancia_naranja
+    "🍊 Naranja": distancia_naranja,
+    "🍇 Uva": distancia_uva
 }
 
 fruta_mas_parecida = min(distancias, key=distancias.get)
